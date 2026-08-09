c692c67da6aa766bc2570ad3a1c1025ec499247f
Eduardo Rodrigues <edu.west009@gmail.com>
2026-08-08 21:36:36 -0300

    First commit of marketplace

---
+++ src/app/login.tsx
@@ -0,0 +1,10 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Login(){
+    return(
+        <View>
+            <Text>Login</Text>            
+        </View>
+    )
+}
